76131bbdcc5ed4b35fa55a2ab33148a08c01463e
wnjoon <wnjoon@gmail.com>
2022-11-02 13:41:37 +0900

    init repository

---
+++ src/main/java/com/example/tinyftx/TinyftxApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyftx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyftxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyftxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinyftx/TinyftxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinyftx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyftxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
